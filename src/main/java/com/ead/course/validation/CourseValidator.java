4c4fca9ba802ffb7662206c984ac2ad04150aa66
Lucas de Assis Mudo <lucas.assis.mudo@gmail.com>
2024-11-12 11:17:06 -0300

    First commit

---
+++ src/main/java/com/ead/course/validation/CourseValidator.java
@@ -0,0 +1,50 @@
+package com.ead.course.validation;
+
+import com.ead.course.dtos.CourseDto;
+import com.ead.course.enums.UserType;
+import com.ead.course.models.UserModel;
+import com.ead.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class CourseValidator implements Validator {
+
+    @Autowired
+    @Qualifier("defaultValidator")
+    private Validator validator;
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        CourseDto courseDto = (CourseDto) target;
+        validator.validate(courseDto, errors);
+        if (!errors.hasErrors()) {
+            validateUserInstructor(courseDto.getUserInstructor(), errors);
+        }
+    }
+
+    private void validateUserInstructor(UUID userInstructor, Errors errors) {
+        Optional<UserModel> userModelOptional = userService.findById(userInstructor);
+        if(!userModelOptional.isPresent()){
+            errors.rejectValue("userInstructor", "UserInstructorError", "Instructor Not Found!");
+        }
+
+        if (userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())){
+            errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN!");
+        }
+    }
+}
